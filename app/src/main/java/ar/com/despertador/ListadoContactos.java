eccd40e14a8bbc7c09f07fd5571efa1acb568516
German Medina <gmedinasistemas@gmail.com>
2021-10-30 04:55:38 -0300

    Agrega Prototipo ListadoContactos
    Tenemos que terminar de definir los colores, de los activity.

---
+++ app/src/main/java/ar/com/despertador/ListadoContactos.java
@@ -0,0 +1,14 @@
+package ar.com.despertador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListadoContactos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_contactos);
+    }
+}
