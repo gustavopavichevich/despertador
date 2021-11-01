d2d986dd34e6d0761782d0e1d8de8b9e3bbc6e64
LeoYermoli <81972081+LeoYermoli@users.noreply.github.com>
2021-11-01 02:17:07 -0300

    Modificacion Leo Agregar pantalla de crear cuenta

---
+++ app/src/main/java/ar/com/despertador/AgregarCuentaActivity.java
@@ -0,0 +1,14 @@
+package ar.com.despertador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgregarCuentaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_cuenta);
+    }
+}
