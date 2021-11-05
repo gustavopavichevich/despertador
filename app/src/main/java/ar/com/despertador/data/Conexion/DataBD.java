34ebf4ef6a16679b4c600de535ae55cf484ae3bc
Gustavo Pavichevich <88042886+guspavichevich-globant@users.noreply.github.com>
2021-11-05 20:16:20 -0300

    Actualizamos la conexion a la base de datos

---
+++ app/src/main/java/ar/com/despertador/data/Conexion/DataBD.java
@@ -0,0 +1,20 @@
+package ar.com.despertador.data.Conexion;
+
+public class DataBD {
+
+    //Información de la BD
+    public static String host = "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    public static String port = "3306";
+    public static String nameBD = "sozsu9g190okokyf";
+    public static String user = "m51bewe8ny5nmu4a";
+    public static String pass = "dot69hwp8zp8ucn6";
+
+    //Información para la conexion
+    public static String urlMySQL = "jdbc:mysql://" + host + ":" + port + "/" + nameBD;
+    public static String driver = "com.mysql.jdbc.Driver";
+}
+
+
+
+
+
