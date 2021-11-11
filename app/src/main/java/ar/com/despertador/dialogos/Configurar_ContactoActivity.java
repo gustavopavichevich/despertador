106703de3233fa738ea534de92c7cf60bf02fa93
German Medina <gmedinasistemas@gmail.com>
2021-11-10 21:38:50 -0300

    Carga de Variables

--- app/src/main/java/ar/com/despertador/dialogos/Configurar_ContactoActivity.java
+++ app/src/main/java/ar/com/despertador/dialogos/Configurar_ContactoActivity.java
@@ -9,6 +9,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 import ar.com.despertador.ConfiguracionAlarmaActivity;
@@ -16,12 +18,19 @@ import ar.com.despertador.R;
 
 public class Configurar_ContactoActivity extends AppCompatActivity {
     static final int PICK_CONTACT_REQUEST=1;
+    RadioButton _radiow, _radiosms;
+    EditText _txtmensaje;
+    String _emailU;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configurar_contacto);
+        _emailU=getIntent().getStringExtra("email");
+        _radiow = (RadioButton)findViewById(R.id.radioWhatsApp);
+        _radiosms = (RadioButton)findViewById(R.id.radioSMS);
+        _txtmensaje = (EditText)findViewById(R.id.txtMensaje);
     }
     public void ElegirContacto(View v)
     {
@@ -33,25 +42,26 @@ public class Configurar_ContactoActivity extends AppCompatActivity {
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        //super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode ==PICK_CONTACT_REQUEST) {
-            if (resultCode == RESULT_OK)
-            {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            if (resultCode == RESULT_OK) {
                 Uri uri = data.getData();
 
-                Cursor cursor= getContentResolver().query(uri,null,null,null,null);
+                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
 
-                if (cursor.moveToFirst()){
-                    int columnaNombre=cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-                    int columnaNumero=cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                if (cursor.moveToFirst()) {
+                    int columnaNombre = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                    int columnaNumero = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
                     String nombre = cursor.getString(columnaNombre);
                     String numero = cursor.getString(columnaNumero);
-                    Toast.makeText(this,"Registro Seleccionado: " + nombre + " Celular: " + numero, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Registro Seleccionado: " + nombre + " Celular: " + numero, Toast.LENGTH_SHORT).show();
                     //grabar los datos del contacto seleccionado en la base
 
 
                     //Voy a configurar Alarma
-                    Intent intent=new Intent(this, ConfiguracionAlarmaActivity.class);
+                    //paso los valores al siguiente activity
+
+                    Intent intent = new Intent(this, ConfiguracionAlarmaActivity.class);
                     startActivity(intent);
 
                 }
