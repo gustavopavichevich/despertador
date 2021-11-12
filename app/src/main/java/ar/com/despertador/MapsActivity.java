7f10f28bfd36ec6faf2bfa39635f32bb44f2a32d
Gustavo Pavichevich <88042886+guspavichevich-globant@users.noreply.github.com>
2021-11-12 15:15:43 -0300

    Limpieza de proyecto y configuración de activity

--- app/src/main/java/ar/com/despertador/MapsActivity.java
+++ app/src/main/java/ar/com/despertador/MapsActivity.java
@@ -4,23 +4,18 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
@@ -30,8 +25,6 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -42,8 +35,7 @@ import java.io.IOException;
 import java.util.List;
 
 import ar.com.despertador.dialogos.Configurar_ContactoActivity;
-import ar.com.despertador.dialogos.DialogoConfigurarContactoFragment;
-import ar.com.despertador.dialogos.DialogoConfigurarRadioFragment;
+import ar.com.despertador.dialogos.Configurar_RadioActivity;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -255,7 +247,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     */
     public void AbrirDialogoConfigRadio() {
 
-        DialogoConfigurarRadioFragment dialogoRadio = new DialogoConfigurarRadioFragment();
-        dialogoRadio.show(getSupportFragmentManager(), "Dialogo Configurar Radio");
+        Intent intent=new Intent(this, Configurar_RadioActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/ar/com/despertador/dialogos/Configurar_RadioActivity.java
+++ app/src/main/java/ar/com/despertador/dialogos/Configurar_RadioActivity.java
@@ -1,82 +1,49 @@
 package ar.com.despertador.dialogos;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import ar.com.despertador.ConfiguracionAlarmaActivity;
 import ar.com.despertador.R;
 
-public class Configurar_RadioActivity extends AppCompatActivity {
-    static final int PICK_CONTACT_REQUEST=1;
-    RadioButton _radiow, _radiosms;
-    EditText _txtmensaje;
-    String _emailU;
+public class Configurar_RadioActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+    private SeekBar mSeekBar;
+    private TextView mProgressText;
+    private TextView mTrackingText;
+    private Button btnCancelarConfCont;
+    private Button btn_AceptarConfCont;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configurar_radio);
-        _emailU=getIntent().getStringExtra("email");
-        _radiow = (RadioButton)findViewById(R.id.radioWhatsApp);
-        _radiosms = (RadioButton)findViewById(R.id.radioSMS);
-        _txtmensaje = (EditText)findViewById(R.id.txtMensaje);
+        mSeekBar = (SeekBar) findViewById(R.id.seek);
+        mSeekBar.setOnSeekBarChangeListener(this);
+        mProgressText = (TextView) findViewById(R.id.progress);
+        mTrackingText = (TextView) findViewById(R.id.tracking);
     }
-    public void ElegirContacto(View v)
-    {
 
-        //validacion de carga de datos
-        if (_radiow.isChecked() && _txtmensaje.getText().toString()!="" || _radiosms.isChecked() && _txtmensaje.getText().toString()!=""){
-            Intent selectContactoIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contactas"));
-            selectContactoIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
-            startActivityForResult(selectContactoIntent,PICK_CONTACT_REQUEST);
-        }
-        else
-        {
-            Toast.makeText(this, "Verifique que tenga selleccion y mensaje asigano", Toast.LENGTH_SHORT).show();
-        }
-    }
     @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_CONTACT_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                Uri uri = data.getData();
+    public void onProgressChanged(SeekBar seekBar, int progress,
+                                  boolean fromUser) {
+        // Durante el arrastre, es decir, el valor está cambiando
+        // el progreso es el tamaño del valor actual
+        mProgressText.setText("Valor actual:" + progress);
+    }
 
-                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // Este método se llamará durante el arrastre
+        mTrackingText.setText("xh se está ajustando");
+    }
 
-                if (cursor.moveToFirst()) {
-                    int columnaNombre = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-                    int columnaNumero = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-                    String nombre = cursor.getString(columnaNombre);
-                    String numero = cursor.getString(columnaNumero);
-                    Toast.makeText(this, "Registro Seleccionado: " + nombre + " Celular: " + numero, Toast.LENGTH_SHORT).show();
-                    //grabar los datos del contacto seleccionado en la base
-
-
-                    //Voy a configurar Alarma
-                    Intent intent = new Intent(this, ConfiguracionAlarmaActivity.class);
-                    //paso los valores al siguiente activity
-                    intent.putExtra("email",_emailU);
-                    intent.putExtra("radiow",_radiow.getText().toString());
-                    intent.putExtra("radiosms",_radiosms.getText().toString());
-                    intent.putExtra("txtmensaje",_txtmensaje.toString());
-                    intent.putExtra("nombre",nombre);
-                    intent.putExtra("numero",numero);
-                    startActivity(intent);
-
-                }
-            }
-        }
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // Dejar de arrastrar
+        mTrackingText.setText("ajuste de parada xh");
     }
 }
