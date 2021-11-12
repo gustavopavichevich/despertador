e34665a7641046f1c240ec8303701f8bcff80a29
LeoYermoli <81972081+LeoYermoli@users.noreply.github.com>
2021-11-11 23:50:55 -0300

    Agregado de searchbar para buscar una direccion en el mapa

--- app/src/main/java/ar/com/despertador/MapsActivity.java
+++ app/src/main/java/ar/com/despertador/MapsActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,6 +16,7 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -34,7 +37,10 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import ar.com.despertador.databinding.ActivityMapsBinding;
+//import ar.com.despertador.databinding.ActivityMapsBinding;
+import java.io.IOException;
+import java.util.List;
+
 import ar.com.despertador.dialogos.Configurar_ContactoActivity;
 import ar.com.despertador.dialogos.DialogoConfigurarContactoFragment;
 import ar.com.despertador.dialogos.DialogoConfigurarRadioFragment;
@@ -49,12 +55,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     double lat = 0.0;
     double log = 0.0;
     //private ActivityMapsBinding binding;
+    SupportMapFragment mapFragment;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
+        searchView=findViewById(R.id.sv_ubicacion);
+        mapFragment=(SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList=null;
+
+                if (location != null || !location.equals("")){
+                    Geocoder geocoder = new Geocoder (MapsActivity.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location,1  );
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        mapFragment.getMapAsync(this);
         //binding = ActivityMapsBinding.inflate(getLayoutInflater());
         //setContentView(binding.getRoot());
 
