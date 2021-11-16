c2c0af0814ed427865bc5a23bcb74711b1f4ca8e
German Medina <gmedinasistemas@gmail.com>
2021-11-16 13:06:17 -0300

    Actualizo Variables ConfiguracionAlarmaActivity

--- app/src/main/java/ar/com/despertador/dialogos/ConfiguracionAlarmaActivity.java
+++ app/src/main/java/ar/com/despertador/dialogos/ConfiguracionAlarmaActivity.java
@@ -131,12 +131,12 @@ public class ConfiguracionAlarmaActivity extends AppCompatActivity {
 
                     alarma.setNombre(_txtnombrealarma.getText().toString());
                     alarma.setUrlTono(_tono);
-                    getIntent().putExtra("tono", _numero);
+                    getIntent().putExtra("tono", _tono);
                     alarma.setMensaje(_txtmensajealarma.getText().toString());
                     getIntent().putExtra("txtMensaje", _txtmensajealarma.getText().toString());
                     alarma.setDistanciaActivacion(radio);//distancia predefinida
                     alarma.setVolumen(_volalarma.getProgress());//obtengo el valor seleccionado
-                    getIntent().putExtra("volumen", _numero);
+                    getIntent().putExtra("volumen", _volalarma.getProgress());
 
                     ubicacion.setPoi(_poiDestino);//la posicion de la busqueda de destino
 
